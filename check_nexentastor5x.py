d22d1fa621f782429deb525696ec0e1003560f67
Sander <sander@Sanders-MacBook-Pro.local>
2019-07-05 22:49:29 +0200

    added new parser

--- check_nexentastor5x.py
+++ check_nexentastor5x.py
@@ -82,38 +82,39 @@ def get_alert_cases(headers, nexenta_api_url, severity, faulty):
         sys.exit(0)
 
 def main():
-    # Variable declaration
-    nexenta_api_url = ''
-    nexenta_api_user = ''
-    nexenta_api_password = ''
+    # Parser
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-H', '--host',
+        type=str, default='127.0.0.1', help='NexentaStor IP / Hostname')
+    parser.add_argument('-P', '--port',
+        type=int, default=8443, help='NexentaStor REST API port')
+    parser.add_argument('-u', '--user',
+        type=str, default='api', help='NexentaStor REST API user')
+    parser.add_argument('-p', '--password',
+        type=str, help='NexentaStor REST API USER password')
+    parser.add_argument('-c', '--check',
+        type=str, choices=['alert', 'log'], help='Check alert or log')
+    parser.add_argument('-s', '--severity',
+        type=str, choices=['minor', 'major', 'critical'], help='Choose severity on alerts')
+    parser.add_argument('-f', '--faulty',
+        type=str, choices=['true', 'false'], help='Show faulty alerts or not')
+
+    args_stdin = parser.parse_args()
+
+    nexenta_api_host = args_stdin.host
+    nexenta_api_port = args_stdin.port
+    nexenta_api_user = args_stdin.user
+    nexenta_api_password = args_stdin.password
+    check = args_stdin.check
+    severity = args_stdin.severity
+    faulty = args_stdin.faulty
+
+    nexenta_api_url = 'https://' + nexenta_api_host + ':' + str(nexenta_api_port)
+
     token = get_token(nexenta_api_url, nexenta_api_user, nexenta_api_password)
     token = token['token']
     headers = {'Authorization': 'Bearer ' + str(token)}
-
-    # Parser
-    parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers()
-
-    parser_alert = subparsers.add_parser('alert',
-        help='Query the API for Alerts'
-    )
-    parser_alert.add_argument("severity",
-        help = "Choose which severity to display: minor, major, critical",
-    )
-    parser_alert.add_argument("faulty",
-        help = "If true gather only active issues"
-    )
-    parser_log = subparsers.add_parser('log',
-        help='Query logs from API'
-    )
-    parser_log.add_argument("severity",
-        help = "Choose which severity to display: info, warning, critical"
-    )
-
-    if sys.argv[1]:
-        stdin = sys.argv[1]
-
-    args_stdin = parser.parse_args()
 
     if token:
         token_status = get_session_status(headers,
@@ -121,7 +122,7 @@ def main():
             nexenta_api_user
         )
         if token_status == 200:
-            if stdin == 'alert':
+            if check == 'alert':
                 severity = args_stdin.severity
                 faulty = args_stdin.faulty
                 if (severity) and (faulty):
@@ -136,7 +137,7 @@ def main():
                         + str(severity) + "or"\
                         + str(faulty) + "not defined"
                     )
-            if stdin == 'log':
+            if check == 'log':
                 print("Option not avalible yet, only testing")
         else:
             print("Failed to authenticate, got: " + str(token_status))
